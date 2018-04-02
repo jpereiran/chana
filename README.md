1bee95b126c122232251867d07c2521ea8c21b55
Jose Pereira <jpereira@pucp.edu.pe>
2018-04-01 21:29:47 -0500

    First commit
    README and lemmatizer

---
+++ README.md
@@ -0,0 +1,80 @@
+Chana: An NLP toolkit the Shipibo-Konibo language of Peru.
+============
+
+chana is a Python module of various NLP tools for the Shipibo-Konibo. 
+Some of these tools can be reused on other peruvian indigenous and/or highly agglutinative languages. 
+It is built on top of scikit-learn and distributed under ... license.
+
+Chana has various NLP tools such as:
+
+* Lemmatizer.
+* Part-of-Speech tagger. 
+* Named Entity annotation.
+* Morphologic Analizer.
+* Spellchecker.
+
+Website: http://chana.inf.pucp.edu.pe
+
+
+Installation
+------------
+
+Dependencies
+~~~~~~~~~~~~
+
+Chana requires:
+
+- Python (>= 3.4)
+- NumPy (>= 1.13.1)
+- Scikit-learn (= 0.18.1)
+
+
+User installation
+~~~~~~~~~~~~~~~~~
+
+If you already have a working installation of numpy and scipy,
+the easiest way to install chana is using ``pip`` ::
+
+    pip install -U chana
+
+
+Important links
+~~~~~~~~~~~~~~~
+
+- Official source code repo: https://github.com/...
+- Download releases: https://pypi.python.org/pypi/...
+
+
+Help and Support
+----------------
+
+Documentation
+~~~~~~~~~~~~~
+
+- HTML documentation (stable release): http://chana.inf.pucp.edu.pe
+
+
+Communication
+~~~~~~~~~~~~~
+
+- Website: http://chana.inf.pucp.edu.pe
+- Authors: **Jose Pereira** - [jpereiran](https://github.com/jpereiran)
+
+
+Contact
+~~~~~~~~
+
+For any question and feedback please contact:
+
+- José Pereira Noriega	&lt;jpereira pucp edu pe&gt;
+- Rodolfo Mercado Gonzales	&lt;rmercado pucp edu pe&gt;
+- Marco Sobrevilla Cabezudo	&lt;msobrevilla pucp edu pe&gt;
+- Arturo Oncevay Marcos	&lt;arturo.oncevay pucp edu pe&gt;
+
+
+Acknowledgments
+~~~~~~~~
+
+- José Pereira Noriega
+- Rodolfo Mercado Gonzales
+
---
+++ setup.py
@@ -0,0 +1,64 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+setup(
+    name='chana',  # Required
+    version='0.1.0.dev1',  # Required
+    description='A module of NLP tools for the shipibo-konibo language',  # Required
+
+    url='https://github.com/jpereiran/chana',  # Optional
+    author='Jose Pereira',  # Optional
+    author_email='jpereira@pucp.edu.pe',  # Optional
+    license='MIT',
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Natural Language Processing :: Various Tools',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    keywords='nlp shipibo development',  # Optional
+    
+    packages= ['chana'],  # Required
+
+    # This field lists other packages that your project depends on to run.
+    # Any package you put here will be installed by pip when your project is
+    # installed, so they must be valid existing projects.
+    #
+    # For an analysis of "install_requires" vs pip's requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['numpy sklearn'],  # Optional
+
+
+    # List additional URLs that are relevant to your project as a dict.
+    #
+    # This field corresponds to the "Project-URL" metadata fields:
+    # https://packaging.python.org/specifications/core-metadata/#project-url-multiple-use
+    #
+    # Examples listed include a pattern for specifying where the package tracks
+    # issues, where the source is hosted, where to say thanks to the package
+    # maintainers, and where to support the project financially. The key is
+    # what's used to render the link text on PyPI.
+    project_urls={  # Optional
+        'Bug Reports': 'https://github.com/pypa/sampleproject/issues',
+        'Funding': 'https://donate.pypi.org',
+        'Say Thanks!': 'http://saythanks.io/to/example',
+        'Source': 'https://github.com/pypa/sampleproject/',
+    },
+)
